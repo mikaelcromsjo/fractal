b6823386796fae6be0c271e6b0f5f50d7530c8b7
Mikael <mikael.cromsjo@gmail.com>
2026-02-26 16:56:52 +0100

    rep-group

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,9 +6,9 @@ class Settings(BaseSettings):
     ENV: str = "development"
     DATABASE_URL: str  # will be read from .env or environment variable
     TEST_DATABASE_URL: str
-    GROUP_SIZE_DEFAULT: int = 5
+    GROUP_SIZE_DEFAULT: int = 7
     PROPOSALS_PER_USER_DEFAULT: int = 2
-    ROUND_TIME_DEFAULT: int = 5
+    ROUND_TIME_DEFAULT: int = 2
 #    public_base_url: str = "https://temptingly-breechless-venessa.ngrok-free.dev"
 #    public_base_wss_url: str = "wss://temptingly-breechless-venessa.ngrok-free.dev"
     public_base_url: str = "https://fractal.ia-ai.se"
